3da5910dad3b7f24cdcbf8d51918f03f58fa7712
Bruce Lefebvre <blefebvr@adobe.com>
2016-09-15 14:46:50 -0400

    Include a "Download PDF" example in the app

--- www/js/index.js
+++ www/js/index.js
@@ -33,5 +33,73 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+    },
+
+    downloadPDF: function(pdfURL) {
+        if (typeof FileTransfer === "undefined") {
+            return alert("FileTransfer plugin is not ready");
+        }
+
+        var dataDirectoryPath;
+        if (cordova.file.externalDataDirectory != null) {
+            // I found this directory works best on Android because other apps
+            // (like Acrobat) have access to read from here.
+            // However, this directory is null on iOS.
+            dataDirectoryPath = cordova.file.externalDataDirectory;
+        }
+        else {
+            // This directory works fine on iOS because the share options all
+            // seem to use a copy of the PDF doc.
+            dataDirectoryPath = cordova.file.dataDirectory;
+        }
+
+        var fileURL = dataDirectoryPath + "downloadedPDF.pdf";
+
+        console.log('downloading file to: ' + fileURL);
+
+        var fileTransfer = new FileTransfer();
+        fileTransfer.download(
+            pdfURL,
+            fileURL,
+            function win(pdfFileEntry) {
+                navigator.notification.confirm(
+                    'PDF successfully downloaded! Open it now?', // message
+                    function(buttonIndex) {
+                        if (buttonIndex === 1) {
+                            // use the cordova-plugin-file-opener2 plugin
+                            cordova.plugins.fileOpener2.open(
+                                pdfFileEntry.toURL(), 
+                                'application/pdf', 
+                                {
+                                    error: function() {
+                                        console.error('Failed to open the PDF');
+                                    }, 
+                                    success: function(){ 
+                                        console.log('Successfully opened the PDF');
+                                    } 
+                                } 
+                            );
+                        }
+                        else if (buttonIndex === 2) {
+                            // "No thanks" was tapped
+                            // no-op
+                        }
+                    }, // callback to invoke with index of button pressed
+                    'Success',           // title
+                    ['Open it','No thanks']     // buttonLabels
+                );
+            },
+            function fail(error) {
+                console.error("download error source " + error.source);
+                console.error("download error target " + error.target);
+                console.error("download error code" + error.code);
+            },
+            false,  // trustAllHosts
+            {
+                headers: {
+                    // optional http headers here
+                }
+            }
+        );
     }
 };
